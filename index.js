b094581fa0b572e66a41f21aadfbb906bc84383f
Hatice <haticekaraty@gmail.com>
2021-07-28 20:55:41 -0400

    commit tree 1372c918704e6f5c37ef46d3970484ffd21989e2

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+    return str[0].toUpperCase() + str.slice(1)
+  }
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+  static titleize(str){
+    let words = str.split(" ")
+    let skipWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    return words.map((word,i) => {
+      //for (const skipWord in skipWords){
+        if (!skipWords.includes(word) || i === 0){
+          return Formatter.capitalize(word)
+        }else{
+          return word
+        }
+      }
+    ).join(" ")
+  }
 }
